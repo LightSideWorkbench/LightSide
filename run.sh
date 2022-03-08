803dfe3228dea01aedaf2ffe46f60e0e298d1039
rcmurray <>
2022-03-08 17:06:47 -0500

    Update run.sh for new LightSide configuration

--- run.sh
+++ run.sh
@@ -18,11 +18,11 @@ fi
 
 MAIN_CLASS="edu.cmu.side.Workbench"
 
-CLASSPATH="maven-local-repository/LightSide.jar"
+JAR="LightSide.jar"
 
 CLASSPATH=""
 
 #CLASSPATH="bin:lib/*:lib/xstream/*:wekafiles/packages/chiSquaredAttributeEval/chiSquaredAttributeEval.jar:wekafiles/packages/bayesianLogisticRegression/bayesianLogisticRegression.jar:wekafiles/packages/LibLINEAR/lib/liblinear-java-1.96-SNAPSHOT.jar:wekafiles/packages/LibLINEAR/LibLINEAR.jar:wekafiles/packages/LibSVM/lib/libsvm.jar:wekafiles/packages/LibSVM/LibSVM.jar:plugins/genesis.jar"
     
-java $OS_ARGS -Xmx$MAXHEAP $OTHER_ARGS -splash:toolkits/icons/logo.png -classpath $CLASSPATH $MAIN_CLASS $@
+java -cp $JAR $OS_ARGS -Xmx$MAXHEAP $OTHER_ARGS -splash:toolkits/icons/logo.png $MAIN_CLASS $@
 
