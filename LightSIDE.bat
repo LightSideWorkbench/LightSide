6a9e6dab294de058fef301421b38ed99c2612eb4
rcmurray <>
2023-08-25 10:43:55 -0400

    Update LightSide.bat file for Windows

--- LightSIDE.bat
+++ LightSIDE.bat
@@ -1,11 +1,11 @@
 @echo off
 (echo Launching LightSide! Console output and errors are being saved to lightside_log.) >CON
 
-set memory=1G
+set memory=8G
 
-set other_args=-XX:+UseConcMarkSweepGC -Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel
+set other_args=-Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel --add-opens java.xml/com.sun.org.apache.xml.internal.serialize=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED
 
-set classpath="bin;plugins/genesis.jar;lib/*;lib/xstream/*"
+set classpath="build/classes/java/main;lib/;lib/plugins/*;lib/plugins/genesis.jar;lib/*"
 
 set wekafiles=wekafiles/packages/chiSquaredAttributeEval/chiSquaredAttributeEval.jar;wekafiles/packages/bayesianLogisticRegression/bayesianLogisticRegression.jar;wekafiles/packages/LibLINEAR/lib/liblinear-java-1.96-SNAPSHOT.jar;wekafiles/packages/LibLINEAR/LibLINEAR.jar;wekafiles/packages/LibSVM/lib/libsvm.jar;wekafiles/packages/LibSVM/LibSVM.jar
 
