48b4f54f5d7e47e99cc3446f54952edfc67e7595
rcmurray <>
2026-05-19 13:57:28 -0400

    Add files for the LightSide HTML page

--- prediction_server.sh
+++ prediction_server.sh
@@ -4,6 +4,7 @@ if type -p java; then
     _java=java  
 elif [[ -n $JAVA_HOME ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
     _java="$JAVA_HOME/bin/java"
+    echo "*** LightSide/prediction_server.sh: Java found ***"
 else
     echo "*** No Java JRE found: Install Java 1.8 or greater and then try again ***"
     exit 1
@@ -22,13 +23,16 @@ IFS=''
 
 if [ $v0 = "1" ] && [ $v1 = "8" ]; then\
     . prediction_server_java8.sh
+    echo "*** LightSide/prediction_server.sh: Using Java8 ***"
 elif [ $v0 = "1" ] && [ $v1 < "8" ]; then
     echo "*** LightSide requires Java 1.8 or greater ***"
     exit 1
 elif [ $v0 = "1" ] && [ $v1 > "8" ]; then
     . prediction_server_java9plus.sh
+    echo "*** LightSide/prediction_server.sh: Using Java9lus ***"
 elif [ $v0 > "1" ]; then
     . prediction_server_java9plus.sh
+    echo "*** LightSide/prediction_server.sh: Using Java9lus ***"
 else 
     echo "*** Invalid Java version ***"
     exit 1
